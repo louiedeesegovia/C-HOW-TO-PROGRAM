6e68648fbc35249440add670180fe1fd81b97905
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-04-22 12:20:16 +0800

    Add files via upload

---
+++ Exercises/Exercises 2/Exercise 2.25.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+	printf("LLLLLLLLLLLLL\n");
+	printf("L\n");
+	printf("L\n");
+	printf("L\n");
+	printf("L\n");
+	printf("\n");
+	printf("DDDDDDDDDDDDD\n");
+	printf("D           D\n");
+	printf("  D        D\n");
+	printf("    D    D\n");
+	printf("       D\n");
+	printf("\n");
+	printf("RRRRRRRRRRRRR\n");
+	printf("     RR     R\n");
+	printf("    R   R  R\n");
+	printf("  R       R\n");
+	printf("R\n");
+	printf("\n");
+	
+	printf("S        SS \n");
+	printf(" S      S   S\n");
+	printf("   S   S     S\n");
+	printf("     S        S\n");
+	
+	return 0;
+}
