0ae57dbfb806010b8e6f049cd66246648b044604
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-04-22 11:32:09 +0800

    Add files via upload

---
+++ Exercises/Exercises 2/Exercise 2.23.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num1, num2, num3;
+	
+	printf("Enter 3 number: ");
+	scanf("%d %d %d", &num1, &num2, &num3);
+	
+	if(num1 > num2){
+		if(num1 > num3){
+			printf("\nGreater number: %d", num1);
+		}
+	}
+	
+	if(num2 > num3){
+		if(num2 > num1){
+			printf("\nGreater number: %d", num2);
+		}
+	}
+	
+	if(num3 > num1){
+		if(num3 > num2){
+			printf("\nGreater number: %d", num3);
+		}
+	}
+	
+	if(num1 < num2){
+		if(num1 < num3){
+			printf("\nLesser number: %d", num1);
+		}
+	}
+	
+	if(num2 < num3){
+		if(num2 < num1){
+			printf("\nLesser number: %d", num2);
+		}
+	}
+	
+	if(num3 < num1){
+		if(num3 < num2){
+			printf("\nLesser number: %d", num3);
+		}
+	}
+	
+	return 0;
+}
