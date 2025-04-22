f64ddfe54079cf4f54756bb254278ab6e5bc2058
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-04-22 11:32:51 +0800

    Add files via upload

---
+++ Exercises/Exercises 2/Exercise 2.24.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int num;
+	printf("Enter a number: ", num);
+	scanf("%d", &num);
+	
+	if(num%2==0){
+		
+		printf("Even");
+		
+	}
+	else{
+			printf("Odd");
+		}
+	return 0;
+}
