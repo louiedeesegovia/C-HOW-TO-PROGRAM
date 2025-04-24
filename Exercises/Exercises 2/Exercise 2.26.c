415c7f067e15decb35c81c45f5c87dc2b8eaa893
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-04-24 21:14:07 +0800

    Add files via upload

---
+++ Exercises/Exercises 2/Exercise 2.26.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	int num1, num2;
+	
+	printf("Enter the first integer: ");
+	scanf("%d", &num1);
+	printf("Enter the second integer: ");
+	scanf("%d", &num2);
+	
+	if(num2 != 0){
+		if(num1%num2==0){
+			printf("%d is a multiple of %d", num1, num2 );
+		}
+		else{
+			printf("%d is NOT a multiple of %d", num2);
+		}
+	}
+	else{
+		printf("Division by zero is not allowed!");
+	}
+		
+	return 0;
+}
