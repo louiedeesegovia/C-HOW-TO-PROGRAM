d8e09735b08b41639ecce8d62e8ac0e1e9773079
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-04-30 06:59:38 +0800

    Add files via upload

---
+++ Exercises/Exercises 3/Exercise 3.14 c.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	/*
+	
+	IF m > v*2
+		// PROCEED BECAUSE GREATER THAN 
+			TWICE THE CURRENT VALUE OF VARIABLE V
+	ELSE 
+		// PROCEED BECAUSE 'NOT' GREATER THAN 
+			TWICE THE CURRENT VALUE OF VARIABLE V."
+	*/
+	return 0;
+}
