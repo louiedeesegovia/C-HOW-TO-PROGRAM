2f2c7081cd7c5333cbd8308f5b6df550881124d0
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-02 10:45:31 +0800

    Add files via upload

---
+++ Figures/Figures 2/Fig.2.2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	printf("Escape sequence\t\tDescription\n");
+	printf("\t\\n\t\tNewline. Position the cursor at the beginning of the next line.\n");
+	printf("\t\\t\t\tHorizontal tab. Move the cursor to the next tab stop.\n");
+	printf("\t\\a\t\tAlert. Sound the system bell.\n");
+	printf("\t\\\\\t\tBackslash. Insert a backslash character in a string.\n");
+	printf("\t\\\t\tDouble qoute. Insert a double-qoute character in a string.\n");
+	
+}
