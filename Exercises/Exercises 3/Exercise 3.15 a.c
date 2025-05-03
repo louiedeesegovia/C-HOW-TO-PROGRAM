a40c76e862b9e7c882ea27af81eec276a7329f6c
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-03 18:02:36 +0800

    Add files via upload

---
+++ Exercises/Exercises 3/Exercise 3.15 a.c
@@ -0,0 +1,19 @@
+/*
+	PSEUDOCODE 
+	START
+	
+	DECLARE VARIABLE: num1, num2, result
+	
+	DISPLAY "Enter number 1: "
+	INPUT num1
+	
+	DISPLAY "Enter number 2: "
+	INPUT num2
+	
+	CALCULATE result = num1 + num2
+	
+	DISPLAY "Result: " + result
+	
+	END
+	
+*/
