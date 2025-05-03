eb3681c830dac98804e71b483f219b889e06b8c4
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-03 18:04:28 +0800

    Add files via upload

---
+++ Exercises/Exercises 3/Exercise 3.15 c.c
@@ -0,0 +1,20 @@
+/*
+	PSEUDOCODE 
+	START
+
+	DECLARE VARIABLES: num, result
+	SET result = 0
+
+	LOOP
+    	DISPLAY "Enter a number (-1 to end): "
+    	INPUT num
+
+    	IF num != -1 THEN
+        	ADD num TO result
+    	END IF
+	END LOOP WHILE num != -1
+
+	DISPLAY "Result: " + result
+
+	END
+*/
