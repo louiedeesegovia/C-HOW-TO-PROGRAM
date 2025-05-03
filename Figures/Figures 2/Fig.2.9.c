1d303282b8cbc13a9ea7ca4ec0c7c196505d503d
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-03 18:43:22 +0800

    Add files via upload

---
+++ Figures/Figures 2/Fig.2.9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("C operation\t\tArithmetic operator\t\tAlgebraic expression\t\tC expression\n");
+    printf("Addition\t\t\t+\t\t\t\tf + 7\t\t\t   f + 7\n");
+    printf("Subtraction\t\t\t-\t\t\t\tp - c\t\t\t   p - c\n");
+    printf("Multiplication\t\t\t*\t\t\t\tbm\t\t\t   b * m\n");
+    printf("Division\t\t\t/\t\t\t\tx/y\t\t\t   x / y\n");
+    printf("Remainder\t\t\t%%\t\t\t\tr mod s\t\t\t   r %% s\n");
+    return 0;
+}
+
+
+
