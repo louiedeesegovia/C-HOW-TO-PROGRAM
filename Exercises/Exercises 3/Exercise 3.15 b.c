6ecbb0b07c12bc80184c4b7ef5d999a5ad14002f
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-03 18:03:31 +0800

    Add files via upload

---
+++ Exercises/Exercises 3/Exercise 3.15 b.c
@@ -0,0 +1,22 @@
+/*
+	PSEUDOCODE 
+	START
+
+	DECLARE VARIABLES: num1, num2
+
+	DISPLAY "Enter number 1: "
+	INPUT num1
+
+	DISPLAY "Enter number 2: "
+	INPUT num2
+
+	IF num1 > num2 THEN
+    	DISPLAY "num1 is greater than num2"
+	ELSE IF num2 > num1 THEN
+    	DISPLAY "num2 is greater than num1"
+	ELSE
+    	DISPLAY "Both numbers are equal"
+	END IF
+
+	END
+*/
