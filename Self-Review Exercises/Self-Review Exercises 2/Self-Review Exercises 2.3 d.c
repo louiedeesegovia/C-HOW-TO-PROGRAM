ca7b56c5ea779bf23b5d75ec1fbbbc7304221ed0
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-04 09:35:02 +0800

    Add files via upload

---
+++ Self-Review Exercises/Self-Review Exercises 2/Self-Review Exercises 2.3 d.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	int num1;
+	
+	printf("Enter a number: ");
+	scanf("%d", &num1);
+	
+	if(num1!=7){
+		
+		printf("The variable number is not equal to 7");
+	}
+	
+	return 0;
+}
