cea8af97557d3cb3959a8bafdbf70f9d97e9d93d
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-06 18:02:24 +0800

    Update Exercise 3.21.c

--- Exercises/Exercises 3/Exercise 3.21.c
+++ Exercises/Exercises 3/Exercise 3.21.c
@@ -1,3 +1,41 @@
+/*
+	PSEUDOCODE 
+	START
+		
+		DECLARE hours (int)
+		DECLARE rate (float), salary (float), salary (float), temp1 (float), temp2 (float), temp3 (float)
+		
+		SET salary, temp1, temp2, temp3 to 0
+		
+		LOOP WHILE hours != -1
+			DISPLAY "Enter # of hours worked (-1 to end): "
+			INPUT hours
+			
+			IF hours != -1 AND hours <= 40 THEN 
+				DISPLAY "Enter hourly rate of the worker ($00.00): "
+				INPUT rate
+				
+				CALCULATE salary = hours * rate
+				
+				DISPLAY "Salary is " + salary 
+			END IF
+			
+			IF hours != -1 AND hours > 40 THEN
+				DISPLAY "Enter hourly rate of the worker ($00.00): "
+				INPUT rate
+				
+				CALCULATE temp1 = hours - 40
+				CALCULATE temp2 = rate / 2
+				CALCULATE temp3 = temp1 * temp2
+				CALCULATE salary = hours * rate + temp3
+				
+				DISPLAY "Salary is " + salary
+			END IF 
+		END WHILE
+	END
+ 
+*/
+
 #include <stdio.h>
 
 int main(){
