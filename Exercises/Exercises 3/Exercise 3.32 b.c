2c1a0ce24020b8942e91271c40e84b72379a4fe3
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-07 07:33:37 +0800

    Add files via upload

---
+++ Exercises/Exercises 3/Exercise 3.32 b.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	int x = 5;
+	int y = 8;
+	
+	if (x == 8){
+		if (x == 5){
+			printf("@@@@@\n");
+		}
+	}
+	else{
+		printf("#####\n");
+		printf("$$$$$\n");
+		printf("&&&&&\n");
+	}
+	
+	return 0;
+	
+}
