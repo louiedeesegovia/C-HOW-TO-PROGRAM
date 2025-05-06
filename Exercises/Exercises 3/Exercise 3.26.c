a852b34ecaa458c996a5e47e53b42bbefc66b2d8
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-06 20:04:33 +0800

    Add files via upload

---
+++ Exercises/Exercises 3/Exercise 3.26.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	int n = 3;
+	
+	printf("A\tA+2\tA+4\tA+6\n");
+	while( n <= 15){
+		printf("%d\t%d\t%d\t%d\n", n++, n+2, n+4, n+6);
+		n++;
+		n++;
+	}
+	
+	return 0;
+}
