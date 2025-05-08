347582135e47f866b7644255f34c02f4e67e6e2c
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-08 22:18:52 +0800

    Add files via upload

---
+++ Exercises/Exercises 3/Exercise 3.31 a.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main(){
+	int x1 = 9;
+	int y1 = 11;
+	int x2 = 11;
+	int y2 = 9;
+	
+	if(x1 < 10){
+		printf("*****\n");
+	}
+	if(y1 > 10){
+		printf("*****\n");
+	}
+	else{
+		printf("#####\n");
+		printf("$$$$$\n");
+	}
+	printf("\n\n");
+	
+	if(x2 < 10){
+		printf("*****\n");
+	}
+	if(y2 > 10){
+		printf("*****\n");
+	}
+	else{
+		printf("#####\n");
+		printf("$$$$$\n");
+	}
+	
+	return 0;
+}
