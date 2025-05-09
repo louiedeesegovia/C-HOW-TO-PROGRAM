4bbccd6b4e74aba2076db93e2fd2f7072530dc07
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-09 12:49:26 +0800

    Add files via upload

---
+++ Exercises/Exercises 3/Exercise 3.35.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+	int num1, num2, num3, num4, num5, num;
+	
+	printf("Enter a five digit number: ");
+	scanf("%d", &num);
+	
+	if(num<10000 || num>99999){
+		printf("Invalid input! Please enter a five-digit number.");
+	}
+	num1 = num/10000;
+	num2 = (num/1000)%10;
+	num3 = (num/100)%10;
+	num4 = (num/10)%10;
+	num5 = num%10;
+	
+	if(num1 == num5 && num2 && num4){
+		printf("The number is a Palindrome.");
+	}
+	else{
+		printf("Not a palindrome!!! Try again!!!");
+	}
+	
+	return 0;
+}
