d327755930245a6a34cfadc59f9832405b1fa94f
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-10 18:48:37 +0800

    Add files via upload

---
+++ Projects/Project 001_global vs local.c
@@ -0,0 +1,14 @@
+//Global and local variable example
+
+#include<stdio.h>
+
+int x = 14;	// Global Variable
+
+int main(){
+	int y = 13; // Local Variable
+	
+	printf("x = %d, y = %d", x, y);
+	
+	return 0;
+	
+}
