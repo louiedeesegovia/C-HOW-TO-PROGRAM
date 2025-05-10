0ec5c008c83e24c99aaaccfa217766d616d523dd
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-10 18:50:15 +0800

    Add files via upload

---
+++ Projects/Project 002_square root.c
@@ -0,0 +1,14 @@
+// find square root of number example
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	int num;
+	
+	printf("Enter a number: ");
+	scanf("%d", &num);
+	
+	printf("Result: %f", sqrt(num));
+	return 0;
+}
