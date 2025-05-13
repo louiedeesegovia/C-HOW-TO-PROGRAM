182e0ee9799cb361be9688f598155def91968dcb
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-05-13 20:58:39 +0800

    Update Exercise 3.39.c

--- Exercises/Exercises 3/Exercise 3.39.c
+++ Exercises/Exercises 3/Exercise 3.39.c
@@ -2,25 +2,32 @@
 
 int main() {
     int num, i = 1;
-	int temp;
-	
+    int temp;
+    int total_sevens = 0; 
+
     printf("Enter a number: ");
     scanf("%d", &num);
 
     while (i <= num) {
         temp = i;
+        int found = 0;
 
         while (temp > 0) {
             if (temp % 10 == 7) {
-                printf("%d\n", i);
-                break;
+                total_sevens++;
+                found = 1; 
             }
             temp /= 10;
+        }
+
+        if (found) {
+            printf("%d\n", i);
         }
 
         i++;
     }
 
+    printf("All Sevens: %d\n", total_sevens);
+
     return 0;
 }
-
