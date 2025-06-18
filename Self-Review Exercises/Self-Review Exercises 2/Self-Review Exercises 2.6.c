5e7dc836208bfef13f6edfac581c0cac13b1409a
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-06-18 18:38:16 +0800

    Add files via upload

---
+++ Self-Review Exercises/Self-Review Exercises 2/Self-Review Exercises 2.6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int number1, number2, c;
+	printf("Value of number 1: ");
+	scanf("%d", &number1);
+	
+	printf("Value of number 2: ");
+	scanf("%d", &number2);
+	
+	
+	
+	if(c<7);{
+	printf("C is less than 7\n");
+	}
+	
+	if (c>=7){
+		printf("C is equal to or less than 7\n");
+	}
+	
+	
+	
+	return 0;
+}
