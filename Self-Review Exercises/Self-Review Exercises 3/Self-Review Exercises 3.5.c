5f22d2ba1bf211ec1602571b475c6b0cf2fe6e0f
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-06-23 17:13:57 +0800

    Add files via upload

---
+++ Self-Review Exercises/Self-Review Exercises 3/Self-Review Exercises 3.5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void){
+	int sum, x;
+	x = 1;
+	sum = 0;
+	
+	
+	while(x <= 10){
+		sum = sum + x;
+		x++;
+	}
+	
+	printf("The sum is: %d\n", sum);
+	
+	
+	return 0;
+}
