645cce47f4c294b2f445acdf54662edee4e3fbb5
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-08-21 19:06:50 +0800

    Add files via upload

---
+++ Figures/Figures 005/Fig. 5.55.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x = 5.7;
+    double result = floor(x);
+    printf("floor(%.1f) = %.1f\n", x, result); // Output: floor(5.7) = 5.0
+    return 0;
+}
+
