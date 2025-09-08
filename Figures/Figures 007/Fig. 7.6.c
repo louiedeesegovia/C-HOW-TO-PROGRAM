87aff568491088264067787d5a582b161c9515df
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-08 19:39:10 +0800

    Add files via upload

---
+++ Figures/Figures 007/Fig. 7.6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int cubeByValue(int n){
+	return n * n * n;
+}
+
+int main(void){
+	int number = 5;
+	
+	printf("The original value of number is %d", number);
+	
+	number = cubeByValue(number);
+	
+	printf("\nThe new value of number is %d\n", number);
+	
+	return 0;
+}
