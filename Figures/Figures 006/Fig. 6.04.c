165b338e47014e38960a949747a8669bc40d2cf5
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-10 09:16:36 +0800

    Add files via upload

---
+++ Figures/Figures 006/Fig. 6.04.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void){
+	int n[10] = { 32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+	int i;
+	
+	printf("%s%13s\n", "Element", "Value");
+	
+	for(i = 0; i < 10; i++){
+		printf("%7d%13d\n", i, n[i]);
+	}
+	
+	return 0;
+}
+
