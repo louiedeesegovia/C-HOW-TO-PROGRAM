14f70a030171f7a84487b3b5909a891aef184349
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2025-09-16 12:21:26 +0800

    Add files via upload

---
+++ Exercises/Exercises 5/Exercise 5.13 b.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int n;
+
+    // seed the random generator
+    srand(time(NULL));
+
+    // generate random integer between 1 and 2
+    n = 1 + rand() % 100;
+
+    printf("Random n: %d\n", n);
+
+    return 0;
+}
+
