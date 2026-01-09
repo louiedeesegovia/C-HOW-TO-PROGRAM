09dd9f497513f2a547fc36a25e270316a6ca6027
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2026-01-09 18:20:45 +0800

    Create GPP 2.01.c

---
+++ Programming Tips/Good Programming Practice/GPP 2.01.c
@@ -0,0 +1,9 @@
+/*
+  Good Programming Practice 2.1
+  Every function should be preceded by a comment describing the purpose of the function.
+*/
+
+// This function calculates the sum of two integers
+int add(int a, int b) {
+    return a + b;
+}
