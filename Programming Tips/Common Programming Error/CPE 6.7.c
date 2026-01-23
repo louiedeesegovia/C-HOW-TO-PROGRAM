fc35b8f536e30fc883300d6b19db308df67a5c66
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2026-01-23 08:59:43 +0800

    Update CPE 6.7.c

--- Programming Tips/Common Programming Error/CPE 6.7.c
+++ Programming Tips/Common Programming Error/CPE 6.7.c
@@ -2,10 +2,8 @@
 Not providing scanf with a character array large enough to store a string typed at the key
 board can result in destruction of data in a program and other runtime errors. This can
 also make a system susceptible to worm and virus attacks.
-*/
 
 //UNSAFE EXAMPLE
-/*
 #include <stdio.h>
 
 int main() {
@@ -17,10 +15,9 @@ int main() {
     printf("Hello, %s\n", name);
     return 0;
 }
-*/
+
 
 //SAFE WITH SIZE LIMIT
-/*
 #include <stdio.h>
 
 int main() {
