51f61e029545774b71e5dfd13cf1a29fbfbf78ad
Louie Dee Segovia <88310635+louiedeesegovia@users.noreply.github.com>
2026-06-19 11:35:50 +0800

    Update Fig. 6.06.c

--- Figures/Figures 006/Fig. 6.06.c
+++ Figures/Figures 006/Fig. 6.06.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 #define SIZE 12
 
-int main(){
-	int a[SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+int main(void){
+
+	int a[SIZE] = {1, 3, 5, 4, 7, 2, 99, 16, 45, 67, 89, 45};
 	
-	int total = 0;
 	int i;
+	int total = 0;
 	
 	for(i = 0; i < SIZE; i++){
 		total += a[i];
 	}
 	
-	printf("Total score: %d", total);
+	printf("Total of array element values is %d\n", total);
 	
 	return 0;
 }
